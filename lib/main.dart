aef60038b88bc4a12b7ccff4e2601ef37e4b035b
guoyiheng <mypridelife@163.com>
2020-01-14 10:39:07 +0800

    add: 毛玻璃，路由等demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hello_provider/example/animation_button.dart';
+import 'package:hello_provider/example/bottom_appbar/bottom_appbar.dart';
+import 'package:hello_provider/example/bottom_appbar/bottom_appbar2.dart';
+import 'package:hello_provider/example/frosted_glass_demo.dart';
+import 'package:hello_provider/example/navigator_demo/bottom_navigation_widget.dart';
+import 'package:hello_provider/example/navigator_demo/navigation_keep_alive.dart';
 import 'package:hello_provider/example/provider_demo.dart';
+import 'package:hello_provider/example/router_transition/pages.dart';
 import 'package:hello_provider/model/CounterModel.dart';
 import 'package:provider/provider.dart';
 
@@ -52,6 +58,36 @@ class RouterPage extends StatelessWidget {
             child: Text("provider demo"),
             onPressed: () => Navigator.of(context)
                 .push(MaterialPageRoute(builder: (context) => FirstScreen())),
+          ),
+          OutlineButton(
+            child: Text("navigator demo"),
+            onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => BottomNavigationWidget())),
+          ),
+          OutlineButton(
+            child: Text("navigator keep alive demo"),
+            onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => NavigationKeepAlive())),
+          ),
+          OutlineButton(
+            child: Text("bottom appbar demo"),
+            onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => BottomAppBarDemo())),
+          ),
+          OutlineButton(
+            child: Text("bottom appbar2 demo"),
+            onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => BottomAppBarDemo2())),
+          ),
+          OutlineButton(
+            child: Text("router transition demo"),
+            onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => FirstPage())),
+          ),
+          OutlineButton(
+            child: Text("FrostedGlass demo"),
+            onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => FrostedGlassDemo())),
           ),
         ],
       ),
